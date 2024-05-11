591e5861d76240dec4103209dc7a3fa4d486c62d
Oscar Avelar Claros <avelarclaros@gmail.com>
2024-05-11 15:37:46 -0600

    Implement de complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalnceBox from "@/components/TotalBalnceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Oscar",
+    lastName: "Avelar",
+    email: "avelarclaros@gmail.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalnceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.5}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
